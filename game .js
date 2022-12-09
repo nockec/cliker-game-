b2eca4a9c5740dc8b53d0c504bf356aa13e501be
nockec <nockec@s.dcsdk12.org>
2022-12-09 16:05:41 -0700

    amoutcliked

---
+++ game .js
@@ -0,0 +1,19 @@
+//cliker game 
+
+let AmountCliked = [];
+let cliked = +1;
+
+
+const button = document.getElementById("guessing-game");
+button?.addEventListener("click", function () {
+while(AmountCliked+1){
+alert("=1");
+
+
+};
+});
+
+
+
+
+
